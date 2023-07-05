cf2405a9f92f4ce92dd7037db367138ad148deb2
BaKYCoder <bakycoder@gmail.com>
2023-07-06 00:37:58 +0200

    Create item class: `Magical Returner`.

---
+++ src/main/java/com/baky/backtobed/item/custom/MagicalReturner.java
@@ -0,0 +1,10 @@
+package com.baky.backtobed.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class MagicalReturner extends Item {
+
+    public MagicalReturner(Properties pProperties) {
+        super(pProperties);
+    }
+}
