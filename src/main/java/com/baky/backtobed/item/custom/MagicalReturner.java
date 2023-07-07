be0e30984611c0005b7b17fef5c2f084cbde6a07
BaKYCoder <bakycoder@gmail.com>
2023-07-07 17:50:39 +0200

    Add bow animation and use consumption to item.

--- src/main/java/com/baky/backtobed/item/custom/MagicalReturner.java
+++ src/main/java/com/baky/backtobed/item/custom/MagicalReturner.java
@@ -3,9 +3,13 @@ package com.baky.backtobed.item.custom;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -34,5 +38,16 @@ public class MagicalReturner extends Item {
         } else {
             tooltipComponents.add(Component.translatable("magical_returner.shift_press").withStyle(ChatFormatting.YELLOW));
         }
+    }
+
+    @Override
+    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
+        return UseAnim.BOW; // This makes the player perform the bow drawing animation while holding right-click
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level worldIn, Player playerIn, @NotNull InteractionHand handIn) {
+        playerIn.startUsingItem(handIn);
+        return InteractionResultHolder.consume(playerIn.getItemInHand(handIn));
     }
 }
