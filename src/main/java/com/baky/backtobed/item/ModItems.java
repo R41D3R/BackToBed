1d65cc2182f7afd16a51706abc5b42d805e14427
BaKYCoder <bakycoder@gmail.com>
2023-07-08 14:29:51 +0200

    The item register in the constructor.

--- src/main/java/com/baky/backtobed/item/ModItems.java
+++ src/main/java/com/baky/backtobed/item/ModItems.java
@@ -7,16 +7,15 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BackToBed.MOD_ID);
 
-    public static final RegistryObject<Item> MAGICAL_RETURNER = ITEMS.register("magical_returner",
-            () -> new MagicalReturner(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1)));
-
     public static void register(IEventBus eventBus) {
+        ITEMS.register("magical_returner",
+                () -> new MagicalReturner(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1)));
+
         ITEMS.register(eventBus);
     }
 }
