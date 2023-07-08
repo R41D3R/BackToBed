d752608005ef4b0bbcf119472e8da12d0b76c1ad
BaKYCoder <bakycoder@gmail.com>
2023-07-08 14:29:09 +0200

    Remove unused methods and variables.

--- src/main/java/com/baky/backtobed/BackToBed.java
+++ src/main/java/com/baky/backtobed/BackToBed.java
@@ -1,45 +1,19 @@
 package com.baky.backtobed;
 
 import com.baky.backtobed.item.ModItems;
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(BackToBed.MOD_ID)
 public class BackToBed
 {
     public static final String MOD_ID = "backtobed";
 
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     public BackToBed()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ModItems.register(modEventBus);
-
-        modEventBus.addListener(this::commonSetup);
+        ModItems.register(FMLJavaModLoadingContext.get().getModEventBus());
 
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-    }
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
     }
 }
