7a7cb5907080ff0e15d9c093f520dddd92f38de5
BaKYCoder <bakycoder@gmail.com>
2023-07-26 11:46:15 +0200

    Rebuild the class to tabs creating.

--- src/main/java/com/baky/backtobed/item/ModItems.java
+++ src/main/java/com/baky/backtobed/item/ModItems.java
@@ -2,20 +2,18 @@ package com.baky.backtobed.item;
 
 import com.baky.backtobed.BackToBed;
 import com.baky.backtobed.item.custom.MagicalReturner;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BackToBed.MOD_ID);
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register("magical_returner",
-                () -> new MagicalReturner(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item> MAGICAL_RETURNER = ITEMS.register("magical_returner",
+            () -> new MagicalReturner(new Item.Properties().stacksTo(1)));
 
-        ITEMS.register(eventBus);
-    }
+    public static void register(IEventBus eventBus) { ITEMS.register(eventBus); }
 }
