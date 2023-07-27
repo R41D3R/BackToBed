db307a062ccc98c47a8eb9ce2e6b82821e45be8a
BaKYCoder <bakycoder@gmail.com>
2023-07-27 21:45:31 +0200

    Override the item usage method for 1.20.

--- src/main/java/com/baky/backtobed/item/custom/MagicalReturner.java
+++ src/main/java/com/baky/backtobed/item/custom/MagicalReturner.java
@@ -1,6 +1,5 @@
 package com.baky.backtobed.item.custom;
 
-import org.joml.Vector3f;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
@@ -23,6 +22,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Vector3f;
 
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -54,27 +54,20 @@ public class MagicalReturner extends Item {
         }
     }
 
-    private void stopUsingItemWithCooldown(ServerPlayer player) {
-        player.stopUsingItem();
-        player.getCooldowns().addCooldown(this, 40);
-    }
-
     @Override
-    public void onUsingTick(ItemStack stack, LivingEntity entity, int count) {
+    public void onUseTick(@NotNull Level pLevel, @NotNull LivingEntity pLivingEntity, @NotNull ItemStack pStack, int pRemainingUseDuration) {
         // Skip if it's client-side or not a player
-        if (!(entity instanceof ServerPlayer player) || player.getCommandSenderWorld().isClientSide()) {
+        if (!(pLivingEntity instanceof ServerPlayer player) || player.getCommandSenderWorld().isClientSide()) {
             return;
         }
 
-        int durationHeld = this.getUseDuration(stack) - count;
+        int durationHeld = this.getUseDuration(pStack) - pRemainingUseDuration;
 
         if (durationHeld < 40) {
             return;
         }
 
-        Level level = player.getCommandSenderWorld();
-
-        if (level.dimension() != Level.OVERWORLD) {
+        if (pLevel.dimension() != Level.OVERWORLD) {
             player.sendSystemMessage(Component.translatable("magical_returner.teleport_for_overworld"));
             stopUsingItemWithCooldown(player);
             return;
@@ -88,7 +81,7 @@ public class MagicalReturner extends Item {
             return;
         }
 
-        if (level.getBlockState(respawnPos).getBlock() instanceof BedBlock) {
+        if (pLevel.getBlockState(respawnPos).getBlock() instanceof BedBlock) {
             double destinationX = respawnPos.getX() + 0.5;
             double destinationY = respawnPos.getY() + 0.6D;
             double destinationZ = respawnPos.getZ() + 0.5;
@@ -112,10 +105,10 @@ public class MagicalReturner extends Item {
                 player.teleportTo(destinationX, destinationY, destinationZ);
             }
 
-            level.playSound(null, respawnPos, SoundEvents.AMETHYST_CLUSTER_HIT, SoundSource.PLAYERS, 1.0F, 1.0F);
+            pLevel.playSound(null, respawnPos, SoundEvents.AMETHYST_CLUSTER_HIT, SoundSource.PLAYERS, 1.0F, 1.0F);
 
             DustColorTransitionOptions dustColorTransitionOptions = new DustColorTransitionOptions(new Vector3f(0.0f, 1.0f, 1.0f), new Vector3f(1.0f, 0.0f, 1.0f), 1.0f);
-            ServerLevel serverLevel = (ServerLevel) level;
+            ServerLevel serverLevel = (ServerLevel) pLevel;
 
             serverLevel.sendParticles(player, dustColorTransitionOptions, true, respawnPos.getX() + 0.5, respawnPos.getY() + 0.6D, respawnPos.getZ() + 0.5, 85, 0.85D, 0.75D, .85D, 0.005D);
 
@@ -125,6 +118,11 @@ public class MagicalReturner extends Item {
             player.sendSystemMessage(Component.translatable("magical_returner.no_access_to_bed"));
             stopUsingItemWithCooldown(player);
         }
+    }
+
+    private void stopUsingItemWithCooldown(ServerPlayer player) {
+        player.stopUsingItem();
+        player.getCooldowns().addCooldown(this, 40);
     }
 
     @Override
